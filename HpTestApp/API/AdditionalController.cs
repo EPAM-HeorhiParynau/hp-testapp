b1f106adac448868b9c3eb20d04ddf827f8b34b3
Heorhi Parynau <Heorhi_Parynau@epam.com>
2018-09-26 21:17:26 +0300

    fix route name issue

--- HpTestApp/API/AdditionalController.cs
+++ HpTestApp/API/AdditionalController.cs
@@ -19,7 +19,7 @@ namespace HpTestApp.API
         }
 
         // GET: api/Additional/5
-        [HttpGet("{id}", Name = "Get")]
+        [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
--- HpTestApp/API/TestController.cs
+++ HpTestApp/API/TestController.cs
@@ -19,7 +19,7 @@ namespace HpTestApp.API
         }
 
         // GET: api/Test/5
-        [HttpGet("{id}", Name = "Get")]
+        [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
